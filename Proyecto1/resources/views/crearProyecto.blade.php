820877b4e355b579f623607a5c8e89bfa64bc27c
ferdinandjr-14 <fpintod2425@politecnics.barcelona>
2025-10-18 22:13:13 +0200

    Algunos arreglos de la pantalla crearProyecto

--- Proyecto1/resources/views/crearProyecto.blade.php
+++ Proyecto1/resources/views/crearProyecto.blade.php
@@ -18,6 +18,13 @@
             background-color: #f4f5f7;
         }
 
+        div,
+        div,
+        ul,
+        li {
+            font-size: 1rem;
+        }
+
         /*    CREAR PROYECTO CONTAINER    */
         main {
             display: flex;
@@ -39,9 +46,9 @@
             height: auto;
             min-height: 700px;
             width: 100%;
-            max-width: 900px;
+            max-width: 1200px;
 
-            margin: 40px auto 0 auto;
+            margin: 40px auto 40px auto;
 
             display: flex;
             flex-direction: column;
@@ -77,7 +84,7 @@
         form>div {
             flex: 1;
             box-sizing: border-box;
-            margin-top: 30px;
+            margin-top: 40px;
             display: grid;
             grid-template-columns: repeat(2, 1fr);
             gap: 10px;
@@ -124,6 +131,7 @@
         input[type="number"],
         input[type="date"] {
             border: 2px solid transparent;
+            color: #9ca3af;
         }
 
         input[type="number"]:hover,
@@ -153,6 +161,10 @@
             background-color: #b3eb65;
         }
 
+        form>div>div:nth-child(2)>a {
+            width: max-content;
+        }
+
         #tareas {
             margin-top: 10px;
             display: flex;
@@ -160,6 +172,12 @@
             gap: 5px;
             overflow: auto;
             max-height: 300px;
+            height: 300px;
+        }
+
+        #tareas>p {
+            color: #636363;
+            font-style: italic;
         }
 
         .tarea {
@@ -167,8 +185,28 @@
             font-size: .8rem;
             border: 1px solid #636363;
             border-radius: 5px;
-            padding: 5px;
             background-color: #ffff;
+
+            padding-left: 10px;
+            height: 40px;
+
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .remove-btn {
+            background: #d42d20;
+            color: #fff;
+            margin-left: auto;
+            height: 100%;
+            border-radius: 0 4px 4px 0;
+            transition: all 400ms;
+        }
+
+        .remove-btn:hover {
+            background: #a8190f;
+            color: #fff;
         }
 
         #add-documento {
@@ -180,6 +218,7 @@
             min-width: 170px;
             height: 1rem;
             padding: 20px;
+            margin-top: auto;
             position: relative;
 
             transition: all 400ms;
@@ -195,7 +234,7 @@
             height: 20px;
         }
 
-        #add-documento>span {
+        #add-documento>p {
             position: absolute;
             top: 50%;
             left: 50%;
@@ -213,23 +252,29 @@
             width: 1.2rem;
         }
 
-        input[type="submit"] {
-            margin: auto 0 0 auto;
-        }
-
         #selected-documents {
             font-style: italic;
             color: #636363;
-            margin-top: 10px;
             max-height: 200px;
             overflow: auto;
 
             list-style: inside;
         }
 
+        #selected-documents-msg {
+            font-style: italic;
+            color: #636363;
+        }
+
         #selected-documents>li {
             font-size: 1rem;
         }
+
+        #add-proyecto-btn {
+            margin: auto 0 0 auto;
+            padding: 7px 25px;
+        }
+
 
         @media (width < 600px) {
             main {
@@ -247,8 +292,14 @@
                 grid-template-columns: 1fr;
             }
 
-            input[type="submit"] {
-                margin-top: 50px;
+            #add-proyecto-btn {
+                margin-top: 70px;
+                margin-bottom: 0;
+            }
+
+            #selected-documents {
+                height: 200px;
+                margin-bottom: 20px;
             }
         }
     </style>
@@ -259,41 +310,36 @@
             <form action="project" method="POST">
                 <a id="quit-btn" href="http://localhost/0616/Proyecto1/public/home">X</a>
                 <label for="titulo"></label>
-                <input type="text" name="titulo" id="titulo" placeholder="TITULO PROYECTO..." required
-                    maxlength="100">
+                <input type="text" name="titulo" id="titulo" placeholder="AÑADIR TÍTULO" required maxlength="100">
                 <div>
                     <div>
                         <label for="fecha-limite">Fecha límite</label>
                         <input type="date" name="fecha-limite" id="fecha-limite" required min="">
 
                         <label for="presupuesto">Presupuesto</label>
-                        <input type="number" name="presupuesto" id="presupuesto" placeholder="€€€">
+                        <input type="number" name="presupuesto" id="presupuesto" placeholder="€€€" step="10">
 
+                        <p id="selected-documents-msg">No se han añadido documentos</p>
+                        <ul id="selected-documents"></ul>
                         <label for="documento" id="add-documento">
                             <input type="file" name="documento" id="documento" multiple="true"
-                                accept=".pdf, .doc, .docx, .odt, .rtf, .txt">
-                            <span>Añadir documentos <img src="../storage/assets/icons/upload.svg" alt="Upload button">
-                            </span>
+                                accept=".pdf, .doc, .docx, .odt, .rtf, .txt, .png, .jpg, .jpeg, .svg, .webp">
+                            <p>Añadir documentos <img src="../storage/assets/icons/upload.svg" alt="Upload button">
+                            </p>
                         </label>
-                        <ul id="selected-documents">No se han añadido documentos</ul>
                     </div>
                     <div>
                         <a href="{{ route('tareas.controller') }}">
-                            <button type="button">Añadir tarea</button>
+                            <button type="button" id="add-tareas-btn">Añadir tarea</button>
                         </a>
                         <div id="tareas">
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
-                            <div class="tarea">TAREA</div>
+                            <p id="add-tareas-msg"></p>
+                            <div class="tarea">TAREA <button class="remove-btn" type="button">X</button></div>
+                            <div class="tarea">TAREA <button class="remove-btn" type="button">X</button></div>
+                            <div class="tarea">TAREA <button class="remove-btn" type="button">X</button></div>
+                            <div class="tarea">TAREA <button class="remove-btn" type="button">X</button></div>
                         </div>
-                        <input type="submit" value="Añadir proyecto">
+                        <input type="submit" value="Añadir proyecto" id="add-proyecto-btn">
                     </div>
                 </div>
             </form>
@@ -301,20 +347,48 @@
     </body>
     <script>
         const selectedDocs = document.querySelector("#selected-documents");
+        const selectedDocsMsg = document.querySelector("#selected-documents-msg")
         const docInput = document.querySelector("#documento");
+
         docInput.addEventListener('change', function(e) {
             if (e.target.files.length > 0) {
-                selectedDocs.textContent = 'Documentos:';
+                selectedDocsMsg.textContent = 'Documentos:';
                 [...e.target.files].forEach(file => {
                     selectedDocs.insertAdjacentHTML("beforeend", `<li>${file.name}</li>`)
                 });
             } else {
-                selectedDocs.textContent = 'No se han añadido documentos'
+                selectedDocsMsg.textContent = 'No se han añadido documentos'
             }
         })
 
         const today = new Date().toISOString().split("T")[0];
-        console.log(new Date().toISOString())
         document.querySelector("#fecha-limite").min = today;
+
+
+
+        const addTareasBtn = document.querySelector("#add-tareas-btn")
+        const tareasContainer = document.querySelector("#tareas")
+        const addTareasMsg = document.querySelector("#add-tareas-msg")
+
+        tareas.addEventListener("click", function(e) {
+            const tarea = document.querySelectorAll('.tarea');
+
+            if (tarea.length < 1) {
+                console.log(tarea)
+                addTareasMsg.textContent = 'No se han añadido documentos'
+                return;
+            }
+
+            addTareasMsg.textContent = "";
+
+            if (e.target.classList.contains("remove-btn")) {
+                e.target.closest(".tarea").remove();
+
+                if (document.querySelectorAll(".tarea").length < 1) {
+                    addTareasMsg.textContent = "No se han añadido documentos";
+                }
+            }
+
+        })
     </script>
 @endsection
