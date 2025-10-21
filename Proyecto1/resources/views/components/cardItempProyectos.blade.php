b15a9322987c68caadad9af4b8633feaa24db6e6
josepguiudev <josep.guiu.dev@gmail.com>
2025-10-21 02:06:07 +0200

    Unificación de recursos de Home, CardItemProyectos. Se crean nuevos JS de estos componentes

--- Proyecto1/resources/views/components/cardItempProyectos.blade.php
+++ Proyecto1/resources/views/components/cardItempProyectos.blade.php
@@ -19,28 +19,28 @@
 <div class="card-contenedor cardProyectoId" data-proyecto='@json($dataProyecto)'>
     <div class="card-interior">
         <div class="card-cabecera">
-            <div style="border: 1px solid rgb(65, 65, 161); display: flex; justify-content: center">
-                <div style="height: 90px; width: 90px; border: 1px solid rgb(170, 24, 37); 
+            <div style="display: flex; justify-content: center">
+                <div style="height: 90px; width: 90px; 
                 display: flex; flex-wrap: wrap; justify-content: space-between; 
                 align-items: center;">
                     <!-- Iconos -->
-                    <div style="border: 1px solid pink; flex: 1; margin: 0.5px; align-items: center">
+                    <div style="flex: 1; margin: 0.5px; align-items: center">
                         <img src="../storage/assets/icons/NOajustes.svg" alt="" style="width: 100%; height: 100%;">
                     </div>
-                    <div style="border: 1px solid pink; flex: 1; margin: 0.5px; margin: 0 5px; align-items: center">
+                    <div style="flex: 1; margin: 0.5px; margin: 0 5px; align-items: center">
                         <img src="../storage/assets/icons/NOhome.svg" alt="" style="width: 100%; height: 100%;">
                     </div>
-                    <div style="border: 1px solid pink; flex: 1; margin: 0.5px; margin: 0 5px; align-items: center">
+                    <div style="flex: 1; margin: 0.5px; margin: 0 5px; align-items: center">
                         <img src="../storage/assets/icons/NOReports.svg" alt="" style="width: 100%; height: 100%;">
                     </div>
                 </div>
             </div>
         </div>
     <div class="card-titulo">
-        <div style="border: 1px solid blueviolet;">
+        <div>
             <h2 class="titulo">{{ $titulo }}</h2>
         </div>
-        <div style="border: 1px solid yellow;" class="padding-right">
+        <div class="padding-right">
             <p class="texto-gris">Fecha de entrega</p>
         </div>
     </div>
@@ -60,44 +60,4 @@
    
 </div>
 
-<script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const cards = document.querySelectorAll('.cardProyectoId');
-
-            cards.forEach(card => {
-                
-                const data = card.dataset.proyecto ? JSON.parse(card.dataset.proyecto) : {};
-                
-                const tareasValidar = card.querySelector('#TareasValidar');
-                const tareasCompletadas = card.querySelector('#TareasCompletadas');
-                const tareasPendientes = card.querySelector('#TareasPendientes');
-
-                let tareaCompletasCuenta = 0;
-                let tareaPendienteCuenta = 0;
-                let tareaValidarCuenta = 0;
-
-                data.tareas.forEach(tarea => {
-                    switch(tarea.estado){
-                    case 0:
-                        tareaPendienteCuenta++;
-                        break;
-                    case 1:
-                        tareaValidarCuenta++;
-                        break;
-                    case 2:
-                        tareaCompletasCuenta++;
-                        break;
-                    default:
-                        break;
-                }
-                });
-                
-                tareasValidar.textContent = "Tareas validadas: " + tareaValidarCuenta;
-                tareasCompletadas.textContent = "Tareas completadas: " + tareaCompletasCuenta;
-                tareasPendientes.textContent = "Tareas pendientes: " + tareaPendienteCuenta;
-
-            });
-
-           
-        });
-</script>
+<script src="{{ url('/js/fillCardItemProyectos.js') }}"></script>
--- Proyecto1/resources/views/homePage.blade.php
+++ Proyecto1/resources/views/homePage.blade.php
@@ -113,7 +113,7 @@
                     </div>
 
                 </div>             
-                <div id="cerrarContenedor" style="height: 100%; width: 3%; background-color: red; display: flex; justify-content: center; padding-top: 0.5%; border-radius: 0 0.75rem 0.75rem 0;">
+                <div id="cerrarContenedor" style="height: 100%; width: 3%; display: flex; justify-content: center; padding-top: 0.5%; border-radius: 0 0.75rem 0.75rem 0;">
                     <img src="../storage/assets/icons/cerrar.png" alt="" style="width: 85%; height: fit-content; cursor: pointer;">
                 </div>
             </div>
@@ -121,99 +121,7 @@
     </div>   
     
     <script src="{{ url('/js/btnHome.js') }}"></script>
-
-    <script>
-            const buttonPlus = document.getElementById('plus');
-            const buttonLess = document.getElementById('less');
-            const contenedorTareas = document.getElementById('contenedorTareasProyecto');
-
-            buttonPlus.addEventListener('click', () => {
-                contenedorTareas.style.display = 'block';
-                buttonPlus.classList.add('oculto');
-                buttonLess.classList.remove('oculto');
-            });  
-
-            buttonLess.addEventListener('click', () => {
-                contenedorTareas.style.display = 'none';
-                buttonPlus.classList.remove('oculto');
-                buttonLess.classList.add('oculto');
-            });  
-        
-    </script>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            //Cojemos el id del contenedor de todos los proyectos
-            const contenedorAllProyectos = document.getElementById('contenedorTodosProyectos');
-            //Buscamos el id de la card a seleccionar
-            const cards = document.querySelectorAll('.cardProyectoId');
-            //Buscamos el id del contenedor que muestra el proyecto específico
-            const contenedorMuestra = document.getElementById('contenedorProyectoEspecifico');
-            //Cojemos el id de la foto de cerrar para volver a Proyectos
-            const btnCerrar = document.getElementById('cerrarContenedor');
-
-            console.log(document.querySelectorAll('.cardProyectoId'));
-            //Creamos un foreach para cada card existente en cardProyectoId y de este modo tener el objeto controlado
-            cards.forEach(card => {
-                    
-                //Por la card obtenida (objeto independiente) haremos un evento click
-                card.addEventListener('click', () => {
-                    const data = card.dataset.proyecto ? JSON.parse(card.dataset.proyecto) : {};
-                    console.log('Card clicked');
-                    console.log(data);
-                    console.log(data.titulo);
-                    console.log(data.descripcion);
-                    console.log(data.estado);
-                    
-                    data.tareas.forEach(tarea => {
-                        console.log(tarea.titulo);
-                        console.log(tarea.descripcion);
-                        tarea.tag.forEach(tag => {
-                            console.log(tag.descripcion);
-                            console.log(tag.color);
-                        });
-                    });
-
-                    const titulo = document.getElementById('tituloProyecto');
-                    titulo.textContent = data.titulo;
-                    const descripcion = document.getElementById('descripcionProyecto');
-                    descripcion.textContent = data.descripcion;
-                    const numTasks = document.getElementById('numeroTareas');
-                    numTasks.textContent = `NUMBER OF TASKS: ${data.tareas.length}`;
-
-                    const contenedorTareas = document.getElementById('contenedorTareasProyecto');
-                    contenedorTareas.innerHTML = ''; // Limpiamos el contenedor antes de agregar nuevas tareas
-                    contenedorTareas.style.display = 'none';
-                                        
-                    data.tareas.forEach(tarea => {                        
-                        const tareaElemento = document.createElement('div');
-                        tareaElemento.classList.add('card-cabecera');
-                        tareaElemento.style.marginBottom = '5px';
-
-                        const tituloTarea = document.createElement('h2');
-                        tituloTarea.classList.add('titulo');
-                        tituloTarea.style.marginLeft = '3%';
-                        tituloTarea.textContent = tarea.titulo;
-                        
-                        tareaElemento.appendChild(tituloTarea);
-                        contenedorTareas.appendChild(tareaElemento);
-                    });
-
-                    //Al hacer click en la card, ocultamos el contenedor de todos los proyectos
-                    contenedorAllProyectos.classList.add('oculto');
-                    //Mostramos el contenedor del proyecto específico
-                    contenedorMuestra.classList.remove('oculto');
-                });
-            });
-
-
-
-            //Abierto el contenedor del proyecto específico parra cerralo 
-            btnCerrar.addEventListener('click', () => {
-                contenedorMuestra.classList.add('oculto');
-                contenedorAllProyectos.classList.remove('oculto');
-            });
-        });
-    </script>
+    <script src="{{ url('/js/btnHideProject.js') }}"></script>
+    <script src="{{ url('/js/clickCardProject.js') }}"></script>
 
 @endsection
