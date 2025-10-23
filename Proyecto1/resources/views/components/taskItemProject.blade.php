8841c31ccc2f40a298b08ff92208f90d6d06aefe
ferdinandjr-14 <fpintod2425@politecnics.barcelona>
2025-10-23 19:56:29 +0200

    Miembros y TaskItem convertidos a componentes

---
+++ Proyecto1/resources/views/components/taskItemProject.blade.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="{{ url('/css/taskItemProject.css') }}">
+
+<div class="task-card">
+    <span>
+        <p>Título Tarea</p>
+        <button class="button-task"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                <path
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+            </svg>
+        </button>
+    </span>
+    <span class="tags-container">
+        <p class="tags">TAG</p>
+        <p class="tags">TAG</p>
+        <p class="tags">TAG</p>
+        <p class="tags">TAG</p>
+    </span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+    <span>
+        <p>Responsable</p>
+        <p>Fecha de entrega</p>
+    </span>
+</div>
--- Proyecto1/resources/views/vistaGlobal.blade.php
+++ Proyecto1/resources/views/vistaGlobal.blade.php
@@ -4,3 +4,32 @@
     <link rel="stylesheet" href="{{ url(path: '/css/styles.css') }}">
     <link rel="stylesheet" href="{{ url('/css/vistaGlobal.css') }}">
 @endpush
+
+@section('content')
+    <main>
+        <div id="tab-container">
+            <button class="tabs-btn btn-active" data-tab="1">Usuarios</button>
+            <button class="tabs-btn" data-tab="2">Solicitudes superUsuario</button>
+            <button class="tabs-btn" data-tab="3">Proyectos</button>
+            <button class="tabs-btn" data-tab="4">Grupos</button>
+        </div>
+        <div id="main-content">
+            <div class="tabs-content content-section-1 content-active">
+                <!-- USUARIOS -->
+
+            </div>
+            <div class="tabs-content content-section-2 content-active">
+                <!-- SOLICITUDES SUPERUSUARIO -->
+
+            </div>
+            <div class="tabs-content content-section-3 content-active">
+                <!-- PROYECTOS -->
+
+            </div>
+            <div class="tabs-content content-section-4 content-active">
+                <!-- GRUPOS -->
+
+            </div>
+        </div>
+    </main>
+@endsection
