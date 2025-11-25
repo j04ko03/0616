a75d4210e9a7c6033f75e608987305d1f83c11e8
Josep Guiu Sillés <jguius2021@cepnet.net>
2025-11-25 20:11:52 +0100

    Cambios

--- Proyecto1/resources/views/components/incidenciaItem.blade.php
+++ Proyecto1/resources/views/components/incidenciaItem.blade.php
@@ -1,10 +1,15 @@
  <link rel="stylesheet" href="{{ url('/css/incidenciaItem.css') }}">
 
- <div class="IncidenciaCard contenedorScroll" style="display: flex; flex-direction: column;">
-    <div>
-        <p>{{ $nombreUser }}</p>
+ <div class="IncidenciaCard contenedorScroll" style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: start;">
+    <div class="" style="display: flex; flex-direction: column; width: 90%">
+        <div>
+            <p>{{ $nombreUser }}</p>
+        </div>
+        <div style="padding-left: 2%">
+            <p>{{ $descripcion }}</p>
+        </div>
     </div>
-    <div>
-        <p>{{ $descripcion }}</p>
+    <div style="width: 10%; height: 10%;">
+        <img id="papelera" src="../storage/assets/icons/papelera.png" alt="foto papelera" style="width: 25%; object-fit: contain">
     </div>
  </div>
