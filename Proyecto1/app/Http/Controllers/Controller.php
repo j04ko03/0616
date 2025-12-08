3d9730aab0749de240dcb5b4e2670ce86f46f2af
josepguiudev <josep.guiu.dev@gmail.com>
2025-12-09 00:45:22 +0100

    Controlador para ejecutar php artisan storage:link en el server

--- Proyecto1/app/Http/Controllers/Controller.php
+++ Proyecto1/app/Http/Controllers/Controller.php
@@ -1,8 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Http\Request;
 
 abstract class Controller
 {
     //
+    public function crearLink()
+    {
+        $exitCode = Artisan::call('storage:link');
+
+        $output = Artisan::output();
+
+        /*return response()->json([
+            'exit_code' => $exitCode,
+            'output' => $output
+        ]);*/
+        return view('auth.signIn');
+    }
 }
